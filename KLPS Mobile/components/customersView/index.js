70a2c7c2a24f1c4915753f88a36a9ee05ef22905
Dave Branson <d.branson@fieldtraks.ca>
2015-12-02 21:38:22 +0000

    Changes made by Dave Branson (d.branson@fieldtraks.ca).

---
+++ KLPS Mobile/components/customersView/index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+app.customersView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_customersView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_customersView
+(function(parent) {
+    var dataProvider = app.data.klpsMobile,
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Customer',
+                dataProvider: dataProvider
+            },
+
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'Name': {
+                            field: 'Name',
+                            defaultValue: ''
+                        },
+                        'Address': {
+                            field: 'Address',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+            serverFiltering: true,
+            serverSorting: true,
+            serverPaging: true,
+            pageSize: 50
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        customersViewModel = kendo.observable({
+            dataSource: dataSource,
+            itemClick: function(e) {
+                app.mobileApp.navigate('#components/customersView/details.html?uid=' + e.dataItem.uid);
+            },
+            
+            addClick: function(e) {
+                
+                var item = {'Name':'Enter Name Here'};
+                 dataSource = customersViewModel.get('dataSource');
+                dataSource.add(item);
+                dataSource.sync();
+                
+                 customersViewModel.set('currentItem', item);
+                app.mobileApp.navigate('#components/customersView/details.html?uid=' + item.uid);
+            },
+            
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = customersViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                if (!itemModel.Name) {
+                    itemModel.Name = String.fromCharCode(160);
+                }
+                customersViewModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
+
+    parent.set('customersViewModel', customersViewModel);
+})(app.customersView);
+
+// START_CUSTOM_CODE_customersViewModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_customersViewModel
---
+++ KLPS Mobile/dataProviders/klpsMobile.js
@@ -0,0 +1,70 @@
+'use strict';
+
+(function() {
+    var provider = app.data.klpsMobile = new Everlive({
+            offlineStorage: true,
+            appId: '2kbnjshcgu7t9jds',
+            url: '//platform.telerik.com/bs-api/v1/',
+            scheme: 'https'
+        }),
+        accessTokenCacheKey = "access_token",
+        providerAuthentication = provider.authentication,
+        providerLogin = provider.Users.login,
+        authentication = {
+            setCachedAccessToken: function(token) {
+                if (localStorage) {
+                    localStorage.setItem(accessTokenCacheKey, JSON.stringify(token));
+                } else {
+                    app[accessTokenCacheKey] = token;
+                }
+            },
+            getCachedAccessToken: function() {
+                if (localStorage) {
+                    return JSON.parse(localStorage.getItem(accessTokenCacheKey));
+                } else {
+                    return app[accessTokenCacheKey];
+                }
+            },
+            getCacheAccessTokenFn: function(callback) {
+                return function cacheAccessToken(data) {
+                    if (data && data.result) {
+                        authentication.setCachedAccessToken(data.result);
+
+                        callback(data);
+                    }
+                };
+            },
+            loadCachedAccessToken: function() {
+                var token = authentication.getCachedAccessToken();
+
+                if (token) {
+                    providerAuthentication.setAuthorization(
+                        token.access_token,
+                        token.token_type,
+                        token.principal_id);
+                }
+            }
+        };
+
+    authentication.loadCachedAccessToken();
+    provider.Users.login = function cacheAccessTokenLogin(
+        email, password, success, error) {
+        providerLogin.call(this, email, password,
+            authentication.getCacheAccessTokenFn(success), error);
+    };
+
+    document.addEventListener('online', function() {
+        app.data.klpsMobile.offline(false);
+        app.data.klpsMobile.sync();
+    });
+
+    document.addEventListener('offline', function() {
+        app.data.klpsMobile.offline(true);
+    });
+
+}());
+
+// START_CUSTOM_CODE_klpsMobile
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_klpsMobile
